ceecf313e1ad34fa19ca0b7bef124f6326890e52
ferraripuppi <habin@hotmail.co.uk>
2020-03-27 23:08:05 +0000

    removes redundant tiles + adds total products tile

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -31,8 +31,7 @@ function App() {
 			<div className="headerBanner">Coronavirus (COVID-19) Research and Development Dashboard</div>
 			<div className="content">
 				<div style={{ flex: '1' }}>
-					<Tile header="Total Vaccine Trials">23</Tile>
-					<Tile header="Total Treatment Trials">45</Tile>
+					<Tile header="Total Vaccine Products">23</Tile>
 					<Tile header="Vaccine Trials by Country">
 						{trialData.map((trial, i) => {
 							return (
