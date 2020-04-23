ee0e517f7e2d97dd6e4fcf203b2dbfb09d89e415
Jonathan <jonathan@pop-os.localdomain>
2020-04-23 16:48:57 -0400

    uses mock data, displays different color depending on treatment/vaccine, popup when clicked with product info

--- src/components/Map/Map.stories.jsx
+++ src/components/Map/Map.stories.jsx
@@ -2,41 +2,72 @@ import React from 'react'
 import Map from './index'
 import Tile from '../../Tile/Tile'
 
-const mockCities = [
+const mocks = [
   {
-    city: 'New York',
-    state: 'New York',
-    latitude: 40.6643,
-    longitude: -73.9385,
+    brandName: '',
+    chemicalName: 'mrna-1273',
+    conditionOrDisease: 'covid-19',
+    countries: 'united states',
+    countryCodes: 'usa',
+    currentStatus: 'phase 1',
+    indication: 'covid-19',
+    interventionType: 'vaccine - prophylactic',
+    moleculeType: 'nucleic acid based therapies/vaccines',
+    notes: 'mrna-based vaccine',
+    numSites: '2',
+    otherPartners: 'cepi',
+    phase: '1',
+    preferredName: 'mrna-1273',
+    productId: 1,
+    productType: '',
+    repurposed: 'new',
+    sponsors: 'moderna,national institute of allergy and infectious diseases',
+    status: 'ongoing',
+    therapeuticApproach: 'unknown',
+    trialId: 'nct04283461',
+    siteLocations: [
+      {
+        name: 'Hospital Pkwy',
+        city: 'San Jose',
+        state: 'California',
+        country: 'USA',
+        lat: 37.239746,
+        lon: -121.802344,
+      },
+    ],
   },
-  { city: 'Los Angeles', latitude: 34.0194, longitude: -118.4108 },
-  { city: 'Chicago', latitude: 41.8376, longitude: -87.6818 },
-  { city: 'Houston', state: 'Texas', latitude: 29.7805, longitude: -95.3863 },
-  { city: 'Phoenix', state: 'Arizona', latitude: 33.5722, longitude: -112.088 },
   {
-    city: 'San Antonio',
-    state: 'Texas',
-    latitude: 29.4724,
-    longitude: -98.5251,
-  },
-  {
-    city: 'San Diego',
-    state: 'California',
-    latitude: 32.8153,
-    longitude: -117.135,
-  },
-  {
-    city: 'San Francisco',
-    state: 'California',
-    latitude: 37.7751,
-    longitude: -122.4193,
-  },
-  { city: 'Columbus', state: 'Ohio', latitude: 39.9848, longitude: -82.985 },
-  {
-    city: 'Seattle',
-    state: 'Washington',
-    latitude: 47.6205,
-    longitude: -122.3509,
+    brandName: 'test',
+    chemicalName: 'mrna-1272',
+    conditionOrDisease: 'covid-19',
+    countries: 'united states',
+    countryCodes: 'usa',
+    currentStatus: 'phase 1',
+    indication: 'covid-19',
+    interventionType: 'treatment',
+    moleculeType: 'nucleic acid based therapies/vaccines',
+    notes: 'mrna-based vaccine',
+    numSites: '2',
+    otherPartners: 'cepi',
+    phase: '1',
+    preferredName: 'mrna-1273',
+    productId: 1,
+    productType: '',
+    repurposed: 'new',
+    sponsors: 'moderna,national institute of allergy and infectious diseases',
+    status: 'ongoing',
+    therapeuticApproach: 'unknown',
+    trialId: 'nct04283461',
+    siteLocations: [
+      {
+        name: 'Test',
+        city: 'Test2',
+        state: 'NY',
+        country: 'USA',
+        lat: 40.6643,
+        lon: -73.9385,
+      },
+    ],
   },
 ]
 
@@ -47,7 +78,7 @@ export default {
 export const DefaultMap = () => (
   <Tile header='Map'>
     <div style={{ height: '88vh', width: '98vw' }}>
-      <Map pins={mockCities} />
+      <Map pins={mocks} />
     </div>
   </Tile>
 )
