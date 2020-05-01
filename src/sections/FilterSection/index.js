a573239bbb5afe23042013f5a26dac5d40a6b403
Jonathan <jonathan@pop-os.localdomain>
2020-05-01 15:31:00 -0400

    get filter from URL. needs to be cleaned up a lot but it's working

--- src/sections/FilterSection/index.js
+++ src/sections/FilterSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from '../../components/Filter/Filter'
 import Tile from '../../components/Tile/Tile'
 import {
@@ -8,20 +8,53 @@ import {
   tourquese,
   green,
 } from '../../constants/colors'
+import { useQueryParams, ArrayParam, withDefault } from 'use-query-params'
 
 const FilterSection = () => {
-  const stages = [
-    { label: 'Stage 1', color: sadBlue, selected: true },
-    { label: 'Stage 2', color: magenta },
-    { label: 'Stage 3', color: yellow },
-    { label: 'Stage 4', color: tourquese },
-    { label: 'Stage 5', color: green },
-  ]
+  const [stages, setStageClicked] = useState([
+    { label: 'Stage 1', color: sadBlue, filterBy: '1' },
+    { label: 'Stage 2', color: magenta, filterBy: '2' },
+    { label: 'Stage 3', color: yellow, filterBy: '3' },
+    { label: 'Stage 4', color: tourquese, filterBy: '4' },
+    { label: 'Stage 5', color: green, filterBy: '5' },
+  ])
+  const [stageParams, setStageParam] = useQueryParams({
+    stages: withDefault(ArrayParam, []),
+  })
+
+  useEffect(() => {
+    // Apply filters from URL on mounting
+    const stagesCopy = [...stages]
+    stagesCopy.forEach(stage => {
+      if (stageParams.stages.includes(stage.filterBy)) {
+        stage.selected = true
+      }
+    })
+    setStageClicked(stagesCopy)
+  }, [])
+
+  const onFilterClick = element => {
+    const stagesCopy = [...stages]
+    const clickedFilter = stagesCopy.filter(
+      filterObj => filterObj.label === element
+    )[0]
+    clickedFilter.selected = !clickedFilter.selected
+    setStageClicked(stagesCopy)
+    const newStageParamFilter = [...stageParams.stages]
+
+    const index = newStageParamFilter.indexOf(clickedFilter.filterBy)
+    if (index > -1) {
+      newStageParamFilter.splice(index, 1)
+    } else {
+      newStageParamFilter.push(clickedFilter.filterBy)
+    }
+    setStageParam({ stages: newStageParamFilter })
+  }
 
   return (
     <Tile>
       <div style={{ position: 'relative', left: '30px' }}>
-        <Filter items={stages} heading='Stages' />
+        <Filter items={stages} heading='Stages' onFilterClick={onFilterClick} />
       </div>
     </Tile>
   )
