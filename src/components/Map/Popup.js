6c616cee0185096263f012210bce35f7d2e4fa20
Jonathan <jonsandersss@gmail.com>
2020-05-09 18:22:05 -0400

    styled, but missing some data

--- src/components/Map/Popup.js
+++ src/components/Map/Popup.js
@@ -3,26 +3,83 @@ import PropTypes from 'prop-types'
 import { Popup } from 'react-map-gl'
 import styled from 'styled-components'
 
+const StyledPopup = styled(Popup)`
+  .mapboxgl-popup-tip {
+    border-bottom-color: #656666 !important;
+  }
+  .mapboxgl-popup-content {
+    padding: 0px;
+  }
+  .mapboxgl-popup-close-button {
+    color: white;
+    font-size: 20px;
+  }
+  min-width: 330px;
+`
+
 const StyledPopupInfo = styled.div`
-  color: blue;
+  /* color: blue; */
   display: flex;
   flex-direction: column;
   align-items: baseline;
 `
 
+const TopContainer = styled.div`
+  background-color: #656666;
+  color: white;
+  width: -webkit-fill-available;
+  text-align: left;
+  padding: 20px;
+`
+
+const DetailsContainer = styled.div`
+  background-color: #222224;
+  width: -webkit-fill-available;
+  text-align: left;
+  padding: 20px;
+`
+
+const Key = styled.div`
+  color: #8b8c8d;
+  min-width: 50%;
+  padding-right: 4px;
+  text-align: right;
+`
+
+const Value = styled.div`
+  color: #fff;
+  min-width: 50%;
+  max-width: 280px;
+  padding-left: 4px;
+`
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 10px;
+`
+
+const StyledButton = styled.button`
+  background-color: #666666;
+  color: #d2d4d5;
+  &:hover {
+    color: #fff;
+  }
+  padding: 10px;
+  border: none;
+  font-size: 14px;
+  width: 220px;
+  cursor: pointer;
+  margin-top: 10px;
+  border-radius: 2px;
+`
+
 const PopUpDisplay = ({ popupInfo, onClose }) => {
   if (popupInfo) {
-    const {
-      chemicalName,
-      clickedLocation,
-      currentStatus,
-      sponsors,
-      status,
-      trialId,
-      brandName,
-    } = popupInfo
+    const { clickedLocation, sponsors, phase, preferredName } = popupInfo
     return (
-      <Popup
+      <StyledPopup
         tipSize={5}
         anchor='top'
         longitude={clickedLocation.lng}
@@ -31,20 +88,45 @@ const PopUpDisplay = ({ popupInfo, onClose }) => {
         onClose={onClose}
       >
         <StyledPopupInfo>
-          {/* TODO: This is pretty unstyled.  Waiting to see what we want to populate this with */}
-          <div>{chemicalName}</div>
-          <div>Status: {currentStatus}</div>
-          <div>
-            Sponsors:{' '}
-            {sponsors.map(sponsor => (
-              <div key={sponsor.sponsorId}>{sponsor.sponsorName}</div>
-            ))}
-          </div>
-          <div>{status}</div>
-          <div>{trialId}</div>
-          <div>{brandName}</div>
+          <TopContainer>
+            <div style={{ fontSize: '20px' }}>{preferredName}</div>
+            <div style={{ paddingTop: '10px' }}>{clickedLocation.name}</div>
+            {/* <div>Address 2
+            </div> */}
+          </TopContainer>
+          <DetailsContainer>
+            <Row>
+              <Key>Phase</Key>
+              <Value>{phase}</Value>
+            </Row>
+            <Row>
+              <Key>Condition</Key>
+              <Value>The Value</Value>
+            </Row>
+            <Row>
+              <Key>Sponsor(s)</Key>
+              <Value>
+                {sponsors.map(sponsor => sponsor.sponsorName).join(', ')}
+              </Value>
+            </Row>
+            <Row>
+              <Key>Study Start</Key>
+              <Value>The Value</Value>
+            </Row>
+            <Row>
+              <Key>Study End</Key>
+              <Value>The Value</Value>
+            </Row>
+            <Row>
+              <Key>Healthy Volunteer</Key>
+              <Value>The Value</Value>
+            </Row>
+            <Row>
+              <StyledButton>LEARN MORE</StyledButton>
+            </Row>
+          </DetailsContainer>
         </StyledPopupInfo>
-      </Popup>
+      </StyledPopup>
     )
   }
   return null
@@ -61,7 +143,10 @@ PopUpDisplay.propTypes = {
     clickedLocation: PropTypes.shape({
       lng: PropTypes.number,
       lat: PropTypes.number,
+      name: PropTypes.string,
     }),
+    phase: PropTypes.string,
+    preferredName: PropTypes.string,
   }),
   onClose: PropTypes.func,
 }
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -10,8 +10,65 @@ import ReactMapGL, {
 import { mapboxApiKey } from '../../constants/config'
 import { FullscreenControlDiv, NavDiv, ScaleControlDiv } from './styles'
 
+const mock = {
+  brandName: '',
+  chemicalName: 'mrna-1273',
+  conditionOrDisease: 'covid-19',
+  countries: 'united states',
+  countryCodes: 'usa',
+  currentStatus: 'phase 1',
+  indication: 'covid-19',
+  interventionType: 'vaccine - prophylactic',
+  milestones: [],
+  moleculeType: 'nucleic acid based therapies/vaccines',
+  notes: 'mrna-based vaccine',
+  numSites: '2',
+  otherPartners: 'cepi',
+  phase: '1',
+  preferredName: 'mrna-1273',
+  productId: 1,
+  productType: '',
+  repurposed: 'new',
+  // siteLocations: (2) [{…}, {…}],
+  sources: [
+    'https://www.nih.gov/news-events/news-releases/nih-…cal-trial-investigational-vaccine-covid-19-begins',
+    'https://www.modernatx.com/modernas-work-potential-vaccine-against-covid-19',
+    'https://clinicaltrials.gov/ct2/show/nct04283461?term=coronavirus+vaccine&draw=2',
+    'https://clinicaltrials.gov/ct2/show/nct04283461?term=coronavirus+vaccine&draw=1',
+    'https://clinicaltrials.gov/ct2/show/nct04283461?term=coronavirus+vaccine&draw=3',
+    'https://clinicaltrials.gov/ct2/show/nct04283461?term=coronavirus+vaccine&draw=4',
+    'https://clinicaltrials.gov/ct2/show/nct04283461?term=coronavirus+vaccine&draw=5',
+    'https://clinicaltrials.gov/ct2/show/nct04283461?term=coronavirus+vaccine&draw=6',
+    'https://clinicaltrials.gov/ct2/show/nct04283461?term=coronavirus+vaccine&draw=7',
+    'https://corona.kpwashingtonresearch.org/',
+  ],
+  sponsors: [
+    {
+      sponsorId: '375ad6c4b12a03acefcf5e9b052423279351910a',
+      sponsorName: 'Moderna',
+    },
+    {
+      sponsorId: '69301ed353817a1eb80b77b32f82e5809965a871',
+      sponsorName: 'National Institute of Allergy and Infectious Disease',
+    },
+  ],
+  status: 'ongoing',
+  therapeuticApproach: 'unknown',
+  trialId: 'nct04283461',
+  clickedLocation: {
+    city: 'Seattle',
+    country: 'US',
+    lat: 47.6169397,
+    lng: -122.329572,
+    locationId: 'a6018262eeb2425434539c72bd2bc1cf527bca54',
+    name: '1730 Minor Ave, Seattle, WA 98101, USA',
+    state: 'WA',
+  },
+}
+
 const Map = ({ pins, handleSelectedId }) => {
-  const [popUp, setPopUp] = useState(null)
+  const [popUp, setPopUp] = useState(mock)
+  console.log(popUp, 'popUp')
   const [viewport, setViewport] = useState({
     latitude: 40.67,
     longitude: -73.94,
