9781db49e74e993d5475719687c1c9a0ca38baa5
Nestor Tobon <nltobon@gmail.com>
2020-06-02 19:07:10 -0400

    add missing dataset link (#50)
    some styles to the link
    
    Co-authored-by: Nestor Tobon (C) <ntobon@splunk.com>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import * as S from './styles'
-import { faqUrl, howYouCanHelpUrl } from 'constants/config'
+import {
+  faqUrl,
+  howYouCanHelpUrl,
+  spreadsheetDataSource,
+} from 'constants/config'
 
 const Navbar = () => {
   return (
@@ -8,16 +12,18 @@ const Navbar = () => {
       <S.TitleContainer>
         <S.Title>COVID-19 R&D Dash</S.Title>
         <S.Divider>|</S.Divider>
-        <S.Subtitle>
-          Full Dataset located here :{' '}
-          <a href='https://link.domain' style={{ color: '#fff' }}>
-            https://link.domain
-          </a>
-        </S.Subtitle>
+        <S.Link target='_blank' href={spreadsheetDataSource}>
+          Full dataset here
+        </S.Link>
       </S.TitleContainer>
       <S.Navbar>
-        <S.Link href={faqUrl}>FAQ</S.Link>
-        <S.Link href={howYouCanHelpUrl}>How you can help</S.Link>
+        <S.Link target='_blank' href={faqUrl}>
+          FAQ
+        </S.Link>
+        <S.Divider>|</S.Divider>
+        <S.Link target='_blank' href={howYouCanHelpUrl}>
+          How you can help
+        </S.Link>
       </S.Navbar>
     </S.Wrapper>
   )
