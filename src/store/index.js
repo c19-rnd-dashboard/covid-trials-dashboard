017d6491b8d598d5c6d04ef6928e993b4bdb347c
Nestor Tobon <nltobon@gmail.com>
2020-07-28 11:02:13 -0400

    patch to use assets from backup (#72)

--- src/store/index.js
+++ src/store/index.js
@@ -62,7 +62,7 @@ const StateProvider = ({ children }) => {
     dispatch({
       type: 'fetchData',
     })
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== 'production-patch') {
       const splitData = splitVaccinesAndTreatments(assets)
       dispatch({ type: 'fetchDataSuccess', payload: splitData })
     } else {
