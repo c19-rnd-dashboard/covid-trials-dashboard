4c1a6cf1fbb0aaf8eb140bd0f9945ec345b396dc
Nestor Luis Tobon Arrieta <ntobon@splunk.com>
2020-10-24 20:25:57 -0300

    rebase with master

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { CustomDrawer } from 'sections/Drawer/Drawer'
-import { CategoryMenu } from 'components/CategoryMenu'
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -55,7 +54,6 @@ const Navbar = () => {
               />
             </StyledLink>
           </div>
-          <CategoryMenu />
         </Toolbar>
       </AppBar>
     </div>
