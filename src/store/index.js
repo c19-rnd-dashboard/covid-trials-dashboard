73f0a562acf550ca694a9d6e13dadb939cbedb5d
Nestor Luis Tobon Arrieta <ntobon@splunk.com>
2020-10-24 22:41:01 -0300

    enable theme button

--- src/store/index.js
+++ src/store/index.js
@@ -49,8 +49,7 @@ const StateProvider = ({ children }) => {
     case 'fetchDataFailure':
       return { ...state, error: action.payload, loading: false }
     case 'tooglePrefersDarkMode':
-      return state // disabled until light theme is ready
-      // return { ...state, prefersDarkMode: !state.prefersDarkMode }
+      return { ...state, prefersDarkMode: !state.prefersDarkMode }
     case TOGGLE_FILTER:
       return {
         ...state,
